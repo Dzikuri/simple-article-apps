d0253b999bfbf0f2cf6bf25653da4a6a997b6b56
unknown <sumiartinisrirahayu@gmail.com>
2023-07-14 02:15:43 +0700

    [CMS] Hide feature user profile dan setting

--- resources/views/includes/admin/header.blade.php
+++ resources/views/includes/admin/header.blade.php
@@ -22,11 +22,11 @@
                         <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        <!-- <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
                         </li>
                         <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
                         </li>
-                        <li class="divider"></li>
+                        <li class="divider"></li> -->
                         <li>
                             <a href="{{route('home')}}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
